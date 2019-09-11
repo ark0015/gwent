de6189de4a6ec7ff9d25193628cfcd1bc4e241b6
Hazboun6 <jeffrey.hazboun@gmail.com>
2019-09-11 13:17:54 -0700

    Added actual test.

--- tests/test_gwent.py
+++ tests/test_gwent.py
@@ -14,5 +14,17 @@ from gwent import utils
 from gwent import waveform
 
 
-class TestGwent():
-    """Tests for `gwent` package."""
+@pytest.fixture
+def response():
+    """Sample pytest fixture.
+
+    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """
+    # import requests
+    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+
+
+def test_content(response):
+    """Sample pytest test function with the pytest fixture as an argument."""
+    # from bs4 import BeautifulSoup
+    # assert 'GitHub' in BeautifulSoup(response.content).title.string
