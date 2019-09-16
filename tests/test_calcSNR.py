4586c2bc43015ea415479d23f43b0e2da99126bd
Hazboun6 <jeffrey.hazboun@gmail.com>
2019-09-16 11:54:00 -0700

    Fixed merge conflict.

--- tests/test_calcSNR.py
+++ tests/test_calcSNR.py
@@ -10,14 +10,8 @@ import gwent.binary as binary
 import gwent.detector as detector
 import gwent.snr as snr
 
-<<<<<<< HEAD
-current_path = os.path.abspath(os.path.dirname(__file__))
-load_directory = os.path.join(current_path,'../docs/LoadFiles/InstrumentFiles/')
-=======
 current_path = os.path.abspath(gwent.__path__[0])
 load_directory = os.path.join(current_path,'LoadFiles/InstrumentFiles/')
-
->>>>>>> a02d57d9d0d8a934aa9becc3de43812fba2d0134
 # Constants and Initial Parameters
 
 '''
@@ -214,24 +208,6 @@ def Get_Instrument(model):
 
     return instrument
 
-<<<<<<< HEAD
-
-# # Setting Up SNR Calculation
-# Uses the variables given and the data range to sample the space either
-# logrithmically or linearly based on the
-# selection of variables. Then it computes the SNR for each value.
-# Returns the variable ranges used to calculate the SNR for each matrix, then returns the SNRs with size of the sample1Xsample2
-#
-
-model = 2
-instrument = Get_Instrument(model)
-source = Get_Source(model)
-[sample_x,sample_y,SNRMatrix] = snr.Get_SNR_Matrix(source, instrument, var_x,
-                                                   sampleRate_x, var_y,
-                                                   sampleRate_y)
-
-=======
->>>>>>> a02d57d9d0d8a934aa9becc3de43812fba2d0134
 #Whole Hog Creation of SNR Matrices and Samples
 
 models = [0,1,2,3,4,5]
