ea5fbe2c9c26c9cce93b00f2f2fc174f8928328e
Hazboun6 <jeffrey.hazboun@gmail.com>
2019-09-28 20:54:57 -0700

    Fixed typo in docstring.

--- gwent/utils.py
+++ gwent/utils.py
@@ -13,9 +13,9 @@ def make_quant(param, default_unit):
     Returns
     -------
         an astropy quantity
-    
+
     Examples
-    -------
+    --------
         self.f0 = make_quant(f0,'MHz')
 
     Notes
