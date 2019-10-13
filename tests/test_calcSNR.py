c259f2e25e14fecbedfebb83d1594814f3a82873
Andrew Kaiser <andrewkaiser@Kaiser70.local>
2019-10-12 21:29:20 -0400

    Adding extensive tests

--- tests/test_calcSNR.py
+++ tests/test_calcSNR.py
@@ -1,216 +1,237 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# # Using `gwent` to Calculate Signal-to-Noise Ratios
+
+# Here we present a tutorial on how to use `gwent` to calculate SNRs for the instrument models currently implemented (LISA, PTAs, aLIGO, and Einstein Telescope) with the signal being an array of coalescing Binary Black Holes.
+
 import numpy as np
-import os
 import astropy.units as u
 
+import pytest
+
 import gwent
-import gwent.binary as binary
-import gwent.detector as detector
-import gwent.snr as snr
+from gwent import binary
+from gwent import detector
+from gwent import snr
+from gwent import snrplot
 
-current_path = os.path.abspath(gwent.__path__[0])
-load_directory = os.path.join(current_path,'LoadFiles/InstrumentFiles/')
-# Constants and Initial Parameters
+# We need to get the file directories to load in the instrument files.
+load_directory = gwent.__path__[0] + '/LoadFiles/InstrumentFiles/'
 
-'''
-Variables:
-    GLOBAL:
-        'T_obs' - Observation Time
-    SOURCE:
-        'M' - Mass (Solar Units)
-        'q' - Mass Ratio
-        'chi1' - Spin1
-        'chi2' - Spin2
-        'z' - Redshift
-    LISA ONLY:
-        'L' - Armlength
-        'A_acc' - Acceleration Noise
-        'A_IMS' - Optical Metrology
-        'f_acc_break_low'
-        'f_acc_break_high'
-        'f_IMS_break'
-    PTAs ONLY:
-        'N_p' - Number of Pulsars
-        'sigma' - Timing Error RMS
-        'cadence' - cadence
-'''
-
-var_y = 'z' #Variable on y-axis
-
-sampleRate_y = 50 #Number of SNRMatrix rows
-
-var_x = 'M' #Variable on x-axis
-
-sampleRate_x = 50 #Number of SNRMatrix columns
-
-#Selects which noise curve:
-#							0 is Einstein Telescope,
-#							1 is aLIGO,
-#							2 is NANOGrav 15yr,
-#							3 is SKA (2030s),
-#							4 is Neil Cornish's,
-#							anything else is the L3 proposal
+#Number of SNRMatrix rows
+sampleRate_y = 10
+#Number of SNRMatrix columns
+sampleRate_x = 10
 
 
-# Source Selection
+# ## Source Selection Function
+# Takes in a an instrument model that dictates reasonable mass ranges for the particular detector mass regime and instantiates a source with the variable ranges limited by the waveform calibration region.
+# The source parameters must be set (ie. M,q,z,chi1,chi2), but one only needs to set the minima and maxima of the selected SNR axes variables.
 
-def Get_Source(model):
-    if model == 0 or model == 1:
-        #M = m1+m2 Total Mass
-        M = 1e2
-        M_min = 1e0
-        M_max = 1e5
-    elif model == 2 or model == 3:
-        #M = m1+m2 Total Mass
-        M = 1e8
-        M_min = 1e7
-        M_max = 1e11
-    else:
-        #M = m1+m2 Total Mass
-        M = 1e6
-        M_min = 1e1
-        M_max = 1e10
+#q = m2/m1 reduced mass
+q = 1.0
+q_min = 1.0
+q_max = 18.0
 
-    #q = m2/m1 reduced mass
-    q = 1.0
-    q_min = 1.0
-    q_max = 18.0
+#Chi = S_i*L/m_i**2, spins of each mass i
+chi1 = 0.0 #spin of m1
+chi2 = 0.0 #spin of m2
+chi_min = -0.85 #Limits of PhenomD for unaligned spins
+chi_max = 0.85
 
-    #Chi = S_i*L/m_i**2, spins of each mass i
-    chi1 = 0.0 #spin of m1
-    chi2 = 0.0 #spin of m2
-    chi_min = -0.85 #Limits of PhenomD for unaligned spins
-    chi_max = 0.85
+z = 3.0 #Redshift
+z_min = 1e-2
+z_max = 1e3
 
-    z = 3.0 #Redshift
-    z_min = 1e-2
-    z_max = 1e3
+@pytest.fixture
+def source_pta():
+    #M = m1+m2 Total Mass
+    M = 1e8
+    M_min = 1e7
+    M_max = 1e11
+    
+    source_pta = binary.BBHFrequencyDomain(M,q,z,chi1,chi2)
+    source_pta.M = [M,M_min,M_max]
+    source_pta.q = [q,q_min,q_max]
+    source_pta.chi1 = [chi1,chi_min,chi_max]
+    source_pta.chi2 = [chi2,chi_min,chi_max]
+    source_pta.z = [z,z_min,z_max]
 
-    source = binary.BBHFrequencyDomain(M,q,chi1,chi2,z)
-    source.M = [M,M_min,M_max]
-    source.q = [q,q_min,q_max]
-    source.chi1 = [chi1,chi_min,chi_max]
-    source.chi2 = [chi2,chi_min,chi_max]
-    source.z = [z,z_min,z_max]
+    return source_pta
 
-    return source
+@pytest.fixture
+def source_space_based():
+    #M = m1+m2 Total Mass
+    M = 1e6
+    M_min = 1e1
+    M_max = 1e10
+    
+    source_space_based = binary.BBHFrequencyDomain(M,q,z,chi1,chi2)
+    source_space_based.M = [M,M_min,M_max]
+    source_space_based.q = [q,q_min,q_max]
+    source_space_based.chi1 = [chi1,chi_min,chi_max]
+    source_space_based.chi2 = [chi2,chi_min,chi_max]
+    source_space_based.z = [z,z_min,z_max]
+
+    return source_space_based
+
+#NANOGrav calculation using 11.5yr parameters https://arxiv.org/abs/1801.01837
+T_obs = 15*u.yr #Observing time in years
+T_obs_min = 5*u.yr
+T_obs_max = 30*u.yr
+
+sigma = 100*u.ns.to('s')*u.s #rms timing residuals in seconds
+sigma_min = 100*u.ns.to('s')*u.s
+sigma_max = 500*u.ns.to('s')*u.s
+
+N_p = 18 #Number of pulsars
+N_p_min = 18
+N_p_max = 40
+
+cadence = 1/(2*u.wk.to('yr')*u.yr) #Avg observation cadence of 1 every 2 weeks in num/year
+cadence_min = 2/u.yr
+cadence_max = 1/(u.wk.to('yr')*u.yr)
+
+@pytest.fixture
+def NANOGrav_WN():
+    NANOGrav_WN = detector.PTA('NANOGrav',T_obs,N_p,sigma,cadence)
+    NANOGrav_WN.T_obs = [T_obs,T_obs_min,T_obs_max]
+    NANOGrav_WN.sigma = [sigma,sigma_min,sigma_max]
+    NANOGrav_WN.N_p = [N_p,N_p_min,N_p_max]
+    NANOGrav_WN.cadence = [cadence,cadence_min,cadence_max]
+    return NANOGrav_WN
+
+@pytest.fixture
+def NANOGrav_WN_RN():
+    NANOGrav_WN_RN = detector.PTA('NANOGrav, WN and RN',T_obs,N_p,sigma,cadence,
+        A_rn=[1e-16,1e-12],alpha_rn=[-1/2,1.25])
+    NANOGrav_WN_RN.T_obs = [T_obs,T_obs_min,T_obs_max]
+    NANOGrav_WN_RN.sigma = [sigma,sigma_min,sigma_max]
+    NANOGrav_WN_RN.N_p = [N_p,N_p_min,N_p_max]
+    NANOGrav_WN_RN.cadence = [cadence,cadence_min,cadence_max]
+    return NANOGrav_WN_RN
+
+        
+sigma = 10*u.ns.to('s')*u.s #rms timing residuals in nanoseconds
+sigma_min = 10*u.ns.to('s')*u.s
+sigma_max = 100*u.ns.to('s')*u.s
+N_p = 20 #Number of pulsars
+cadence = 1/(u.wk.to('yr')*u.yr) #Avg observation cadence of 1 every week in num/year
+        
+@pytest.fixture
+def SKA():
+    SKA = detector.PTA('SKA',T_obs,N_p,sigma,cadence)
+    SKA.T_obs = [T_obs,T_obs_min,T_obs_max]
+    SKA.sigma = [sigma,sigma_min,sigma_max]
+    SKA.N_p = [N_p,N_p_min,N_p_max]
+    SKA.cadence = [cadence,cadence_min,cadence_max]
+    return SKA
+        
+
+#L3 proposal
+#Default Params from https://arxiv.org/abs/1702.00786
+T_obs = 4*u.yr #Observing time in years
+T_obs_min = 1*u.yr
+T_obs_max = 10*u.yr
+
+L = 2.5e9*u.m #armlength in meters
+L_min = 1.0e7*u.m
+L_max = 1.0e11*u.m
+
+A_acc = 3e-15*u.m/u.s/u.s
+A_acc_min = 1e-16*u.m/u.s/u.s
+A_acc_max = 1e-14*u.m/u.s/u.s
+
+f_acc_break_low = .4*u.mHz.to('Hz')*u.Hz
+f_acc_break_low_min = .1*u.mHz.to('Hz')*u.Hz
+f_acc_break_low_max = 1.0*u.mHz.to('Hz')*u.Hz
+
+f_acc_break_high = 8.*u.mHz.to('Hz')*u.Hz
+f_acc_break_high_min = 1.*u.mHz.to('Hz')*u.Hz
+f_acc_break_high_max = 10.*u.mHz.to('Hz')*u.Hz
+
+f_IFO_break = 2.*u.mHz.to('Hz')*u.Hz
+f_IFO_break_min = 1.*u.mHz.to('Hz')*u.Hz
+f_IFO_break_max = 5.*u.mHz.to('Hz')*u.Hz
+
+A_IFO = 10e-12*u.m
+A_IFO_min = 1.0e-12*u.m
+A_IFO_max = 2.0e-11*u.m
+
+Background = False
+T_type = 'N'
+    
+@pytest.fixture
+def LISA_ESA():
+    LISA_ESA = detector.SpaceBased('LISA_ESA',T_obs,L,A_acc,f_acc_break_low,f_acc_break_high,A_IFO,f_IFO_break,Background=Background,T_type=T_type)
+    LISA_ESA.T_obs = [T_obs,T_obs_min,T_obs_max]
+    LISA_ESA.L = [L,L_min,L_max]
+    LISA_ESA.A_acc = [A_acc,A_acc_min,A_acc_max]
+    LISA_ESA.f_acc_break_low = [f_acc_break_low,f_acc_break_low_min,f_acc_break_low_max]
+    LISA_ESA.f_acc_break_high = [f_acc_break_high,f_acc_break_high_min,f_acc_break_high_max]
+    LISA_ESA.A_IFO = [A_IFO,A_IFO_min,A_IFO_max]
+    LISA_ESA.f_IFO_break = [f_IFO_break,f_IFO_break_min,f_IFO_break_max]
+    return LISA_ESA
 
 
-# Model Selection
+# ## SNR Calculation
+# ### Global Source Params for all Fiducial Detectors
+# 
+# * 'M' - Mass (Solar Units)
+# * 'q' - Mass Ratio
+# * 'chi1' - Dimensionless Spin of Black Hole 1
+# * 'chi2' - Dimensionless Spin of Black Hole 2
+# * 'z' - Redshift
 
-def Get_Instrument(model):
-    if model == 0: #Einstein Telescope
-        load_name = 'ET_D_data.txt'
-        load_location = load_directory + 'EinsteinTelescope/StrainFiles/' + load_name
+# ### Global Detector Params
+# * 'T_obs' - Detector Observation Time
 
-        T_obs = 4*u.yr #Observing time in years
-        T_obs_min = 1*u.yr
-        T_obs_max = 10*u.yr
+#Variable on x-axis
+var_x = 'M'
+# ## Create of SNR Matrices and Samples for all models
 
-        instrument = detector.GroundBased('ET',T_obs,load_location=load_location,I_type='A')
-        instrument.T_obs = [T_obs,T_obs_min,T_obs_max]
+# ### PTA Only Params
+# 
+# * 'N_p' - Number of Pulsars
+# * 'sigma' - Root-Mean-Squared Timing Error
+# * 'cadence' - Observation Cadence
+#Variable on y-axis
+var_ys_ptas = ['q','chi1','chi2','T_obs','N_p','sigma','cadence']
+def test_NANOGrav_WN_params(source_pta,NANOGrav_WN):
+    for var_y in var_ys_ptas:
+        [sample_x,sample_y,SNRMatrix] = snr.Get_SNR_Matrix(source_pta,NANOGrav_WN,
+                                                           var_x,sampleRate_x,
+                                                           var_y,sampleRate_y)
+def test_NANOGrav_WN_RN_params(source_pta,NANOGrav_WN_RN):
+    for var_y in var_ys_ptas:
+        [sample_x,sample_y,SNRMatrix] = snr.Get_SNR_Matrix(source_pta,NANOGrav_WN_RN,
+                                                           var_x,sampleRate_x,
+                                                           var_y,sampleRate_y)
 
-    elif model == 1: #aLIGO
-        load_name = 'aLIGODesign.txt'
-        load_location = load_directory + 'aLIGO/StrainFiles/' + load_name
-
-        T_obs = 4*u.yr #Observing time in years
-        T_obs_min = 1*u.yr
-        T_obs_max = 10*u.yr
-
-        instrument = detector.GroundBased('aLIGO',T_obs,load_location=load_location,I_type='A')
-        instrument.T_obs = [T_obs,T_obs_min,T_obs_max]
-
-    elif model == 2: #NANOGrav 15 yr
-        ###############################################
-        #NANOGrav calculation using 11.5yr parameters https://arxiv.org/abs/1801.01837
-        T_obs = 15*u.yr #Observing time in years
-        T_obs_min = 10*u.yr
-        T_obs_max = 30*u.yr
-
-        sigma = 100*u.ns.to('s')*u.s #rms timing residuals in seconds
-
-        N_p = 18 #Number of pulsars
-
-        cadence = 1/(2*u.wk.to('yr')*u.yr) #Avg observation cadence of 1 every 2 weeks in num/year
-
-        instrument = detector.PTA('NANOGrav',T_obs,N_p,sigma,cadence)
-        instrument.T_obs = [T_obs,T_obs_min,T_obs_max]
+def test_SKA_params(source_pta,SKA):
+    for var_y in var_ys_ptas:
+        [sample_x,sample_y,SNRMatrix] = snr.Get_SNR_Matrix(source_pta,SKA,
+                                                           var_x,sampleRate_x,
+                                                           var_y,sampleRate_y)
 
 
-    elif model == 3: #SKA (2030s)
-        ###############################################
-        #SKA calculation using parameters and methods from arXiv:0804.4476 section 7.1
-        T_obs = 15*u.yr #Observing time (years)
-        T_obs_min = 10*u.yr
-        T_obs_max = 30*u.yr
+# ### LISA Only Params
+# 
+# * 'L' - Detector Armlength
+# * 'A_acc' - Detector Acceleration Noise
+# * 'A_IFO' - Detector Optical Metrology Noise
+# * 'f_acc_break_low' - The Low Acceleration Noise Break Frequency
+# * 'f_acc_break_high' - The High Acceleration Noise Break Frequency
+# * 'f_IFO_break' - The Optical Metrology Noise Break Frequency
+var_ys_LISA = ['q','chi1','chi2','T_obs','L','A_acc','A_IFO','f_acc_break_low','f_acc_break_high','f_IFO_break']
 
-        sigma = 10*u.ns.to('s')*u.s #rms timing residuals in nanoseconds
+def test_LISA_params(source_space_based,LISA_ESA):
+    for var_y in var_ys_LISA:
+        [sample_x,sample_y,SNRMatrix] = snr.Get_SNR_Matrix(source_space_based,LISA_ESA,
+                                                           var_x,sampleRate_x,
+                                                           var_y,sampleRate_y)
 
-        N_p = 20 #Number of pulsars
 
-        cadence = 1/(u.wk.to('yr')*u.yr) #Avg observation cadence of 1 every week in num/year
 
-        instrument = detector.PTA('SKA',T_obs,N_p,sigma,cadence)
-        instrument.T_obs = [T_obs,T_obs_min,T_obs_max]
 
-    elif model == 4: #Robson,Cornish,and Liu 2018, LISA (https://arxiv.org/pdf/1803.01944.pdf)
-        T_obs = 4*u.yr #Observing time in years
-        T_obs_min = 1*u.yr
-        T_obs_max = 10*u.yr
-
-        L = 2.5e9*u.m #armlength in meters
-        L_min = 1.0e7*u.m
-        L_max = 1.0e11*u.m
-
-        A_acc = 3e-15*u.m/u.s/u.s #M/s**2
-        A_IMS = 1.5e-11*u.m
-        f_IMS_break = 2.*u.mHz.to('Hz')*u.Hz
-        f_acc_break_low = .4*u.mHz.to('Hz')*u.Hz
-        f_acc_break_high = 8.*u.mHz.to('Hz')*u.Hz
-        Background = False
-
-        instrument = detector.SpaceBased('LISA_Alt',
-                                         T_obs,L,A_acc,f_acc_break_low,
-                                         f_acc_break_high,A_IMS,f_IMS_break,
-                                         Background=Background,T_type='A')
-        instrument.T_obs = [T_obs,T_obs_min,T_obs_max]
-        instrument.L = [L,L_min,L_max]
-
-    else: #L3 proposal
-        #Default Params!
-        T_obs = 4*u.yr #Observing time in years
-        T_obs_min = 1*u.yr
-        T_obs_max = 10*u.yr
-
-        L = 2.5e9*u.m #armlength in meters
-        L_min = 1.0e7*u.m
-        L_max = 1.0e11*u.m
-
-        f_acc_break_low = .4*u.mHz.to('Hz')*u.Hz
-        f_acc_break_high = 8.*u.mHz.to('Hz')*u.Hz
-        f_IMS_break = 2.*u.mHz.to('Hz')*u.Hz
-        A_acc = 3e-15*u.m/u.s/u.s
-        A_IMS = 10e-12*u.m
-        Background = False
-
-        instrument = detector.SpaceBased('LISA_ESA',
-            T_obs,L,A_acc,f_acc_break_low,
-            f_acc_break_high,A_IMS,f_IMS_break,
-            Background=Background,T_type='N')
-        instrument.T_obs = [T_obs,T_obs_min,T_obs_max]
-        instrument.L = [L,L_min,L_max]
-
-    return instrument
-
-#Whole Hog Creation of SNR Matrices and Samples
-
-models = [0,1,2,3,4,5]
-for model in models:
-    instrument = Get_Instrument(model)
-    source = Get_Source(model)
-    [sample_x,sample_y,SNRMatrix] = snr.Get_SNR_Matrix(source,instrument,var_x,
-                                                       sampleRate_x,var_y,
-                                                       sampleRate_y)
---
+++ tests/test_waterfall.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import numpy as np
+import astropy.units as u
+
+import pytest
+
+import gwent
+from gwent import binary
+from gwent import detector
+from gwent import snr
+
+# We need to get the file directories to load in the instrument files.
+load_directory = gwent.__path__[0] + '/LoadFiles/InstrumentFiles/'
+
+#Set axes variables and sizes
+
+#Variable on y-axis
+var_y = 'z'
+#Number of SNRMatrix rows
+sampleRate_y = 75
+#Variable on x-axis
+var_x = 'M'
+#Number of SNRMatrix columns
+sampleRate_x = 75
+
+
+#q = m2/m1 reduced mass
+q = 1.0
+q_min = 1.0
+q_max = 18.0
+
+#Chi = S_i*L/m_i**2, spins of each mass i
+chi1 = 0.0 #spin of m1
+chi2 = 0.0 #spin of m2
+chi_min = -0.85 #Limits of PhenomD for unaligned spins
+chi_max = 0.85
+
+z = 3.0 #Redshift
+z_min = 1e-2
+z_max = 1e3
+
+@pytest.fixture
+def source_ground_based():
+    #M = m1+m2 Total Mass
+    M = 1e8
+    M_min = 1e7
+    M_max = 1e11
+    
+    source_ground_based = binary.BBHFrequencyDomain(M,q,z,chi1,chi2)
+    source_ground_based.M = [M,M_min,M_max]
+    source_ground_based.q = [q,q_min,q_max]
+    source_ground_based.chi1 = [chi1,chi_min,chi_max]
+    source_ground_based.chi2 = [chi2,chi_min,chi_max]
+    source_ground_based.z = [z,z_min,z_max]
+
+    return source_ground_based
+
+@pytest.fixture
+def source_pta():
+    #M = m1+m2 Total Mass
+    M = 1e8
+    M_min = 1e7
+    M_max = 1e11
+    
+    source_pta = binary.BBHFrequencyDomain(M,q,z,chi1,chi2)
+    source_pta.M = [M,M_min,M_max]
+    source_pta.q = [q,q_min,q_max]
+    source_pta.chi1 = [chi1,chi_min,chi_max]
+    source_pta.chi2 = [chi2,chi_min,chi_max]
+    source_pta.z = [z,z_min,z_max]
+
+    return source_pta
+
+@pytest.fixture
+def source_space_based():
+    #M = m1+m2 Total Mass
+    M = 1e6
+    M_min = 1e1
+    M_max = 1e10
+    
+    source_space_based = binary.BBHFrequencyDomain(M,q,z,chi1,chi2)
+    source_space_based.M = [M,M_min,M_max]
+    source_space_based.q = [q,q_min,q_max]
+    source_space_based.chi1 = [chi1,chi_min,chi_max]
+    source_space_based.chi2 = [chi2,chi_min,chi_max]
+    source_space_based.z = [z,z_min,z_max]
+
+    return source_space_based
+
+# #### LISA Proposal 1
+# 
+# SNR values from the ESA L3 proposal run.
+
+#L3 proposal
+#Default Params from https://arxiv.org/abs/1702.00786
+
+def test_LISA_prop1(source_space_based):
+    T_obs = 4*u.yr #Observing time in years
+    L = 2.5e9*u.m #armlength in meters
+    A_acc = 3e-15*u.m/u.s/u.s
+    f_acc_break_low = .4*u.mHz.to('Hz')*u.Hz
+    f_acc_break_high = 8.*u.mHz.to('Hz')*u.Hz
+    f_IMS_break = 2.*u.mHz.to('Hz')*u.Hz
+    A_IMS = 10e-12*u.m
+    Background = False
+    T_type = 'N'
+    LISA_prop1 = detector.SpaceBased('LISA_ESA',T_obs,L,A_acc,
+        f_acc_break_low,f_acc_break_high,A_IMS,f_IMS_break,
+        Background=Background,T_type=T_type)
+    [lisa_sample_x,lisa_sample_y,lisa_SNR] = snr.Get_SNR_Matrix(source_space_based,LISA_prop1,var_x,sampleRate_x,
+        var_y,sampleRate_y)
+
+
+# #### Einstein Telescope
+# SNR values from the Einstein Telescope proposal run.
+#Einstein Telescope
+def test_ET(source_ground_based):
+    ET_filedirectory = load_directory + '/EinsteinTelescope/StrainFiles/'
+    ET_filename = 'ET_D_data.txt'
+    ET_filelocation = ET_filedirectory + ET_filename
+    T_obs = 4*u.yr #Observing time in years
+    ET = detector.GroundBased('ET',T_obs,load_location=ET_filelocation,I_type='A')
+    [et_sample_x,et_sample_y,et_SNR] = snr.Get_SNR_Matrix(source_ground_based,ET,var_x,sampleRate_x,var_y,sampleRate_y)
+
+
+# #### aLIGO
+# SNR values from the Advanced LIGO run.
+#aLIGO
+def test_aLIGO(source_ground_based):
+    aLIGO_filedirectory = load_directory + '/aLIGO/StrainFiles/'
+    aLIGO_filename = 'aLIGODesign.txt'
+    aLIGO_filelocation = aLIGO_filedirectory + aLIGO_filename
+    T_obs = 4*u.yr #Observing time in years
+    aLIGO = detector.GroundBased('aLIGO',T_obs,load_location=aLIGO_filelocation,I_type='A')
+    [aLIGO_sample_x,aLIGO_sample_y,aLIGO_SNR] = snr.Get_SNR_Matrix(source_ground_based,aLIGO,var_x,sampleRate_x,var_y,sampleRate_y)
+
+
+# #### NANOGrav
+# SNR values from the NANOGrav-esque run.
+#NANOGrav calculation using 11.5yr parameters https://arxiv.org/abs/1801.01837
+#rms timing residuals in nanoseconds to seconds
+sigma_nano = 100*u.ns.to('s')*u.s
+T_nano = 15*u.yr #Observing time in years
+N_p_nano = 18 #Number of pulsars
+#Avg observation cadence of 1 every 2 weeks in number/year
+cadence_nano = 1/(2*u.wk.to('yr')*u.yr)
+
+def test_NANOGrav_11yr(source_pta):
+    load_name = 'NANOGrav_11yr_S_eff.txt'
+    load_location = load_directory + '/NANOGrav/StrainFiles/' + load_name
+    T_obs = 11.42*u.yr #Observing time in years
+    nanograv = detector.PTA('NANOGrav 11yr',T_obs,load_location=load_location,I_type='E')
+    [nanograv_sample_x,nanograv_sample_y,nanograv_SNR] = snr.Get_SNR_Matrix(source_pta,nanograv,var_x,sampleRate_x,var_y,sampleRate_y)
+
+#NANOGrav with White Noise only
+def test_pta_NANOGrav_WN(source_pta):
+    NANOGrav_WN = detector.PTA('NANOGrav, WN Only',
+        T_nano,N_p_nano,sigma_nano,cadence_nano)
+    [NANOGrav_WN_sample_x,NANOGrav_WN_sample_y,NANOGrav_WN_SNR] = snr.Get_SNR_Matrix(source_pta,NANOGrav_WN,var_x,sampleRate_x,var_y,sampleRate_y)
+
+#NANOGrav with White and Varied Red Noise
+def test_pta_NANOGrav_WN_RN(source_pta):
+    NANOGrav_WN_RN = detector.PTA('NANOGrav, WN and RN',
+        T_nano,N_p_nano,sigma_nano,cadence_nano,
+        A_rn=[1e-16,1e-12],alpha_rn=[-3/4,1])
+    [NANOGrav_WN_RN_sample_x,NANOGrav_WN_RN_sample_y,NANOGrav_WN_RN_SNR] = snr.Get_SNR_Matrix(source_pta,NANOGrav_WN_RN,var_x,sampleRate_x,var_y,sampleRate_y)
+
+# #### SKA
+# SNR values from the SKA-esque run.
+#SKA calculation using parameters and methods from https://arxiv.org/abs/0804.4476 section 7.1
+def test_SKA(source_pta):
+    T_obs = 15*u.yr #Observing time (years)
+    sigma = 10*u.ns.to('s')*u.s #rms timing residuals in nanoseconds
+    N_p = 20 #Number of pulsars
+    cadence = 1/(u.wk.to('yr')*u.yr) #Avg observation cadence of 1 every week in num/year
+
+    SKA = detector.PTA('SKA',T_obs,N_p,sigma,cadence)
+    [SKA_sample_x,SKA_sample_y,SKA_SNR] = snr.Get_SNR_Matrix(source_pta,SKA,var_x,sampleRate_x,var_y,sampleRate_y)
+
+
+
+
+
