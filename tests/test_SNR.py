91b1596e86da58efd87d04c16342f27c15f8b44a
ark0015 <ark0015@mix.wvu.edu>
2021-01-04 14:02:19 -0500

    Including necessary imports

--- tests/test_SNR.py
+++ tests/test_SNR.py
@@ -13,14 +13,14 @@ import pytest
 
 import os, sys
 
-"""
+
 current_path = os.getcwd()
 splt_path = current_path.split("/")
 top_path_idx = splt_path.index("Research")
 top_directory = "/".join(splt_path[0 : top_path_idx + 1])
 gwent_path = top_directory + "/gwent/"
 sys.path.insert(0, gwent_path)
-"""
+
 
 import gwent
 from gwent import binary
