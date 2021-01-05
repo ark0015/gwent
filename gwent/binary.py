Updating tests for new functionality, fixing minor bugs with SNR calculation, added ability to change galactic background noise model and opening angle of space based detectors, moved most of the remaining waveform calculations in binary to waveform.

gwent/binary.py
@@ -10,10 +10,6 @@ import gwent
 from . import waveform
 from . import utils
 
-current_path = os.path.abspath(gwent.__path__[0])
-load_directory = os.path.join(current_path, "LoadFiles/")
-
-
 class BinaryBlackHole:
     """Base Class for frequency domain strains from Binary Black Holes.
 
@@ -70,6 +66,11 @@ class BinaryBlackHole:
 
     @q.setter
     def q(self, value):
+        if hasattr(self,'approximant'):
+            #Need to recalculate pyphenomD if q is changed
+            if self.approximant == 'pyPhenomD' and any([hasattr(self, "_phenomD_f"), hasattr(self, "_phenomD_h")]):
+                del self._phenomD_f
+                del self._phenomD_h
         self.var_dict = ["q", value]
         self._q = self._return_value
 
@@ -207,13 +208,11 @@ class BBHFrequencyDomain(BinaryBlackHole):
             self.f_low = 1e-6
         if not hasattr(self, "approximant"):
             if hasattr(self, "lalsuite_kwargs"):
-                self.approximant = self.lalsuite_kwargs['approximant']
+                self.approximant = self.lalsuite_kwargs["approximant"]
             else:
                 self.approximant = "pyPhenomD"
         if not hasattr(self, "lalsuite_kwargs"):
             self.lalsuite_kwargs = {}
-
-        self.Get_Fitcoeffs()
 
     @property
     def chi1(self):
@@ -221,6 +220,11 @@ class BBHFrequencyDomain(BinaryBlackHole):
 
     @chi1.setter
     def chi1(self, value):
+        if hasattr(self,'approximant'):
+            #Need to recalculate pyphenomD if chi1 is changed
+            if self.approximant == 'pyPhenomD' and any([hasattr(self, "_phenomD_f"), hasattr(self, "_phenomD_h")]):
+                del self._phenomD_f
+                del self._phenomD_h
         self.var_dict = ["chi1", value]
         self._chi1 = self._return_value
 
@@ -230,6 +234,11 @@ class BBHFrequencyDomain(BinaryBlackHole):
 
     @chi2.setter
     def chi2(self, value):
+        if hasattr(self,'approximant'):
+            #Need to recalculate pyphenomD if chi2 is changed
+            if self.approximant == 'pyPhenomD' and any([hasattr(self, "_phenomD_f"), hasattr(self, "_phenomD_h")]):
+                del self._phenomD_f
+                del self._phenomD_h
         self.var_dict = ["chi2", value]
         self._chi2 = self._return_value
 
@@ -286,25 +295,18 @@ class BBHFrequencyDomain(BinaryBlackHole):
     def h_f(self):
         if not hasattr(self, "_h_f"):
             # Default assumes output is in observer frame
-            if self.approximant == "pyPhenomD":
-                if not all([hasattr(self, "_phenomD_f"), hasattr(self, "_phenomD_h")]):
-                    self.Get_PhenomD_Strain()
-                [_, self._h_f] = waveform.Strain_Conv(
-                    self, self._phenomD_f, self._phenomD_h
+            if not hasattr(self, "_h_f"):
+                [self._f, self._h_f] = waveform.Get_Waveform(
+                    self,
+                    approximant=self.approximant,
+                    lalsuite_kwargs=self.lalsuite_kwargs,
                 )
             else:
-                if not hasattr(self, "_h_f"):
-                    [self._f, self._h_f] = waveform.Get_Waveform(
-                        self,
-                        approximant=self.approximant,
-                        lalsuite_kwargs=self.lalsuite_kwargs,
-                    )
-                else:
-                    [_, self._h_f] = waveform.Get_Waveform(
-                        self,
-                        approximant=self.approximant,
-                        lalsuite_kwargs=self.lalsuite_kwargs,
-                    )
+                [_, self._h_f] = waveform.Get_Waveform(
+                    self,
+                    approximant=self.approximant,
+                    lalsuite_kwargs=self.lalsuite_kwargs,
+                )
         return self._h_f
 
     @h_f.deleter
@@ -315,43 +317,23 @@ class BBHFrequencyDomain(BinaryBlackHole):
     def f(self):
         if not hasattr(self, "_f"):
             # Default assumes output is in observer frame
-            if self.approximant == "pyPhenomD":
-                if not all([hasattr(self, "_phenomD_f"), hasattr(self, "_phenomD_h")]):
-                    self.Get_PhenomD_Strain()
-                [self._f, _] = waveform.Strain_Conv(
-                    self, self._phenomD_f, self._phenomD_h
+            if not hasattr(self, "_h_f"):
+                [self._f, self._h_f] = waveform.Get_Waveform(
+                    self,
+                    approximant=self.approximant,
+                    lalsuite_kwargs=self.lalsuite_kwargs,
                 )
             else:
-                if not hasattr(self, "_h_f"):
-                    [self._f, self._h_f] = waveform.Get_Waveform(
-                        self,
-                        approximant=self.approximant,
-                        lalsuite_kwargs=self.lalsuite_kwargs,
-                    )
-                else:
-                    [self._f, _] = waveform.Get_Waveform(
-                        self,
-                        approximant=self.approximant,
-                        lalsuite_kwargs=self.lalsuite_kwargs,
-                    )
+                [self._f, _] = waveform.Get_Waveform(
+                    self,
+                    approximant=self.approximant,
+                    lalsuite_kwargs=self.lalsuite_kwargs,
+                )
         return self._f
 
     @f.deleter
     def f(self):
         del self._f
-
-    def Get_Fitcoeffs(self):
-        """Loads Quasi-Normal Mode fitting files for speed later."""
-        fit_coeffs_filedirectory = os.path.join(
-            load_directory, "PhenomDFiles/fitcoeffsWEB.dat"
-        )
-        self._fitcoeffs = np.loadtxt(fit_coeffs_filedirectory)
-
-    def Get_PhenomD_Strain(self):
-        """Gets the BBH's frequency and waveform from IMRPhenomD."""
-        if not hasattr(self, "_fitcoeffs"):
-            self.Get_Fitcoeffs()
-        [self._phenomD_f, self._phenomD_h] = waveform.Get_PyPhenomD(self)
 
 
 class BBHTimeDomain(BinaryBlackHole):

gwent/detector.py
@@ -1042,8 +1042,14 @@ class SpaceBased(Interferometer):
         Picks the transfer function generation method
         'N' uses the numerically approximated method in Robson, Cornish, and Liu, 2019
         'A' uses the analytic fit in Larson, Hiscock, and Hellings, 2000
-    Background : Boolean, optional
+    Background : Boolean, optional 
         Add in a Galactic Binary Confusion Noise
+    Background_model : int, {0,1}
+        Used to selectGalactic Binary Confusion Noise model: 0 is the Cornish and Robson 2017 model
+        with corrections from Schmitz, Kai 2020 (https://www.mdpi.com/2073-8994/12/9/1477),
+        and 1 is a model of the Galactic confusion noise parameterized as a function of T_obs
+    openingangle : int,float,Quantity, optional
+        Determines the opening angle of the instrument, if unassigned use the value in in Robson, Cornish, and Liu, 2019.
 
     """
 
@@ -1055,6 +1061,11 @@ class SpaceBased(Interferometer):
                 self.T_type = value
             elif keys == "Background":
                 self.Background = value
+            elif keys == "Background_model":
+                self.Background = True
+                self.Background_model = value
+            elif keys == "openingangle":
+                self.openingangle = value
 
         if not hasattr(self, "nfreqs"):
             self.nfreqs = int(1e3)
@@ -1062,7 +1073,10 @@ class SpaceBased(Interferometer):
             self.f_low = 1e-5 * u.Hz
         if not hasattr(self, "f_high"):
             self.f_high = 1.0 * u.Hz
-        if not hasattr(self, "Background"):
+        if hasattr(self, "Background"):
+            if not hasattr(self, "Background_model"):
+                self.Background_model = 0
+        else:
             self.Background = False
 
         if len(args) != 0:
@@ -1227,13 +1241,14 @@ class SpaceBased(Interferometer):
         )
         self.fT = LISA_Transfer_Function_f[idx_f_5:idx_f_1]
 
-    def Get_Analytic_Transfer_Function(self, openingangle=None):
+    def Get_Analytic_Transfer_Function(self):
         """Response function approximation from Calculation described by Cornish, Robson, Liu 2019
-        Parameters
-        ----------
-        openingangle : int,float,Quantity, optional
-        Determines the opening angle of the instrument, if unassigned use the value in the above paper.
+        Uses openingangle property to determine the opening angle of the instrument, if unassigned use the value in the above paper.
         """
+        if hasattr(self,'openingangle'):
+            openingangle = self.openingangle
+        else:
+            openingangle = None
         self.fT = (
             np.logspace(
                 np.log10(self.f_low.value), np.log10(self.f_high.value), self.nfreqs
@@ -1269,18 +1284,15 @@ class SpaceBased(Interferometer):
         if self._T_type == "analytic":
             self.Get_Analytic_Transfer_Function()
 
-    def Add_Background(self, model=0):
+    def Add_Background(self):
         """
         Galactic confusions noise parameters for 6months, 1yr, 2yr, and 4yr
         corresponding to array index 0,1,2,3 respectively
-
-        Parameters
-        ----------
-        model : int, {0,1}
-        Used to select between the Cornish and Robson 2017 model with corrections from Schmitz, Kai 2020 (https://www.mdpi.com/2073-8994/12/9/1477),
-        and a model of the Galactic confusion noise parameterized as a function of T_obs, 0 and 1, respectively.
+        Uses Background_model to select between Galactic Binary Confusion Noise models: 0 is the Cornish and Robson 2017 model
+        with corrections from Schmitz, Kai 2020 (https://www.mdpi.com/2073-8994/12/9/1477),
+        and 1 is a model of the Galactic confusion noise parameterized as a function of T_obs
         """
-        if model == 0:
+        if self.Background_model == 0:
             """
             Galactic confusions noise parameters for 6months, 1yr, 2yr, and 4yr
             corresponding to array index 0,1,2,3 respectively
@@ -1314,7 +1326,7 @@ class SpaceBased(Interferometer):
                 )
                 * (1 + np.tanh(gfkf.to("").value))
             )  # White Dwarf Background Noise
-        elif model == 1:
+        elif self.Background_model == 1:
             """
             Galactic confusions noise parameters as a function of T_obs
             """

gwent/snr.py
@@ -28,9 +28,9 @@ def Get_SNR_Matrix(
         y-axis variable
     sample_rate_y : array
         samples at which SNRMatrix was calculated corresponding to the y-axis variable
-    inc : 
-    integral_consts : 
-    method : 
+    inc :
+    integral_consts :
+    method :
 
     Returns
     -------
@@ -405,17 +405,16 @@ def Calc_Mono_SNR(source, instrument, inc=None, method="PN"):
         scale = 1.0
 
     indxfgw = np.abs(instrument.fT - source.f_gw).argmin()
-    if indxfgw == 0 or indxfgw >= len(instrument.fT)-1:
-        #The source frequency is assumed to be outside the instrument's frequency, thus the SNR is ~0.
-        #print(f"Your assigned source GW frequency is {source.f_gw} and the instrument frequency range is [{np.unique(np.min(instrument.fT))[0]:.1e},{np.unique(np.max(instrument.fT))[0]:.1e}]")
+    if indxfgw == 0 or indxfgw >= len(instrument.fT) - 1:
+        # The source frequency is assumed to be outside the instrument's frequency, thus the SNR is ~0.
+        # print(f"Your assigned source GW frequency is {source.f_gw} and the instrument frequency range is [{np.unique(np.min(instrument.fT))[0]:.1e},{np.unique(np.max(instrument.fT))[0]:.1e}]")
         return 1e-30
     else:
         return (
             scale
             * source.h_gw
             * np.sqrt(
-                np.max(np.unique(instrument.T_obs.to("s")))
-                / instrument.S_n_f[indxfgw]
+                np.max(np.unique(instrument.T_obs.to("s"))) / instrument.S_n_f[indxfgw]
             )
         )
 

gwent/snrplot.py
@@ -597,7 +597,7 @@ def Get_Axes_Labels(
         ]
     elif var == "A_IFO":
         # Proposed Value = 10^{-12}: L3 LISA
-        if var_scale == 'log':
+        if var_scale == "log":
             ax_dict[var_axis + "ticks"] = np.log10(orig_labels)
             ax_dict[var_axis + "label"] = r"$A_{\mathrm{IFO}}$ [m]"
             ax_dict[var_axis + "ticklabels"] = [
@@ -607,14 +607,16 @@ def Get_Axes_Labels(
             scale = 10 ** round(np.log10(min(orig_labels)))
             new_labels = (
                 np.arange(
-                    round(min(orig_labels) / scale), round(max(orig_labels) / scale) + 1, 3
+                    round(min(orig_labels) / scale),
+                    round(max(orig_labels) / scale) + 1,
+                    3,
                 )
                 * scale
             )
             ax_dict[var_axis + "ticks"] = new_labels
             ax_dict[var_axis + "label"] = r"$A_{\mathrm{IFO}}$ [pm]"
             ax_dict[var_axis + "ticklabels"] = [
-                r"$%.1f$" % (x) for x in new_labels/scale
+                r"$%.1f$" % (x) for x in new_labels / scale
             ]
     elif var == "f_acc_break_low":
         # Proposed Value = 0.4mHz: L3 LISA

gwent/waveform.py
@@ -3,12 +3,16 @@ import astropy.constants as const
 import astropy.units as u
 from astropy.cosmology import WMAP9 as cosmo
 
+import os
 import lal
 import lalsimulation
 
 import gwent
 from . import utils
+from . import binary
 
+current_path = os.path.abspath(gwent.__path__[0])
+load_directory = os.path.join(current_path, "LoadFiles/")
 
 def Get_Waveform(
     source,
@@ -46,11 +50,13 @@ def Get_Waveform(
     """
     if approximant == "pyPhenomD":
         if not hasattr(source, "_fitcoeffs"):
-            source.Get_Fitcoeffs()
-        [natural_f, natural_h] = Get_PyPhenomD(source, pct_of_peak=pct_of_peak)
-        return Strain_Conv(source, natural_f, natural_h, out_frame=out_frame)
-    elif approximant in dir(lalsimulation) or isinstance(approximant,int):
-        if isinstance(approximant,int):
+            Get_Fitcoeffs(source)
+        if not all([hasattr(source, "_phenomD_f"), hasattr(source, "_phenomD_h")]):
+            [source._phenomD_f, source._phenomD_h] = Get_PyPhenomD(source, pct_of_peak=pct_of_peak)
+
+        return Strain_Conv(source, source._phenomD_f, source._phenomD_h, out_frame=out_frame)
+    elif approximant in dir(lalsimulation) or isinstance(approximant, int):
+        if isinstance(approximant, int):
             approx = approximant
         else:
             approx = getattr(lalsimulation, approximant)
@@ -66,7 +72,7 @@ def Get_Waveform(
         if not hasattr(source, "f_T_obs"):
             if not hasattr(source, "instrument"):
                 # No instrument? We set some time observed
-                binary.Check_Freq_Evol(source, T_obs=4.0 * u.yr)
+                binary.Check_Freq_Evol(source, T_evol=4.0 * u.yr)
             else:
                 binary.Check_Freq_Evol(source)
         # Uses source f_T_obs (observed frequency T_obs before merger) in detector frame so we convert to source frame
@@ -226,6 +232,12 @@ def Get_Proper_Freq_Params(waveform_dict):
     waveform_dict.update({"deltaF": out_deltaF, "f_min": out_f_min, "f_max": out_f_max})
     return waveform_dict
 
+def Get_Fitcoeffs(source):
+    """Loads Quasi-Normal Mode fitting files for speed later."""
+    fit_coeffs_filedirectory = os.path.join(
+        load_directory, "PhenomDFiles/fitcoeffsWEB.dat"
+    )
+    source._fitcoeffs = np.loadtxt(fit_coeffs_filedirectory)
 
 def Get_Amp_Phase(h):
     """Separates the amplitude and phase from complex strain polarizations (hcross,hplus)."""

tests/test_strain.py
@@ -11,6 +11,7 @@ import astropy.units as u
 import gwent
 from gwent import detector
 from gwent import binary
+from gwent import waveform
 
 load_directory = gwent.__path__[0] + "/LoadFiles/InstrumentFiles/"
 
@@ -330,7 +331,8 @@ def test_PTA_init_error_kwargs():
 
 L = 2.5 * u.Gm  # armlength in Gm
 L = L.to("m")
-LISA_T_obs = 4 * u.yr
+LISA_T_obs_1 = 1 * u.yr
+LISA_T_obs_2 = 3 * u.yr
 
 #### LISA Proposal 1
 # Values taken from the ESA L3 proposal, Amaro-Seaone, et al., 2017 (https://arxiv.org/abs/1702.00786)
@@ -347,7 +349,7 @@ Background = False
 def LISA_prop1():
     LISA_prop1 = detector.SpaceBased(
         "LISA",
-        LISA_T_obs,
+        LISA_T_obs_1,
         L,
         A_acc,
         f_acc_break_low,
@@ -372,7 +374,7 @@ Background = True
 def test_LISA_prop2():
     LISA_prop2 = detector.SpaceBased(
         "LISA Approximate",
-        LISA_T_obs,
+        LISA_T_obs_2,
         L,
         A_acc,
         f_acc_break_low,
@@ -380,8 +382,11 @@ def test_LISA_prop2():
         A_IMS,
         f_IMS_break,
         Background=Background,
+        Background_model = 1,
+        openingangle = 60,
         T_type="A",
     )
+    LISA_prop2.S_n_f;
 
 
 # ####################################################################
@@ -419,6 +424,23 @@ x1 = [0.95, 0.0, -0.95]
 x2 = [0.95, 0.0, -0.95]
 z = [3.0, 0.093, 20.0]
 
+def test_BBH_arg_error():
+    with pytest.raises(ValueError):
+        binary.BBHFrequencyDomain(M[0],q[0],z[0],x1[0],x2[0],M[1])
+
+def test_BBH_base_not_implemented_error():
+    err_source_1 = binary.BinaryBlackHole(M[0],q[0],z[0],x1[0],x2[0])
+    with pytest.raises(NotImplementedError):
+        err_source_1.h_f
+    with pytest.raises(NotImplementedError):
+        err_source_1.f
+
+def test_BBH_base_load_data_error():
+    with pytest.raises(IOError):
+        err_source_2 = binary.BinaryBlackHole(M[0],q[0],z[0],x1[0],x2[0],load_location='./error')
+    with pytest.raises(ValueError):
+        err_source_3 = binary.BinaryBlackHole(M[0],q[0],z[0],x1[0],x2[0])
+        err_source_3.Load_Data()
 
 def test_BBHStrain(LISA_prop1, aLIGO, SKA_WN, ET):
     source_1 = binary.BBHFrequencyDomain(
@@ -432,29 +454,170 @@ def test_BBHStrain(LISA_prop1, aLIGO, SKA_WN, ET):
         f_high=10,
         nfreqs=int(1e3),
     )
-    source_2 = binary.BBHFrequencyDomain(
-        M[1], q[1], z[1], x1[1], x2[1], instrument=aLIGO
-    )
+    source_2 = binary.BBHFrequencyDomain(M[1], q[1], z[1], chi2=x2[1], instrument=aLIGO)
     source_3 = binary.BBHFrequencyDomain(
         M[2], q[2], z[2], x1[2], x2[2], instrument=SKA_WN
     )
-    source_4 = binary.BBHFrequencyDomain(M[1], q[0], z[1], x1[1], x2[1], instrument=ET)
+    source_4 = binary.BBHFrequencyDomain(M[1], q[0], z[1], chi1=x1[1], instrument=ET)
     binary.Get_Char_Strain(source_1)
-    source_2.Get_Time_From_Merger(1 / u.yr, frame="source").to("yr")
+    binary.Get_Time_From_Merger(
+        source_2, freq=1 / u.yr, in_frame="source", out_frame="observer"
+    ).to("yr")
+    binary.Get_Time_From_Merger(
+        source_2, freq=1 / u.yr, in_frame="source", out_frame="source"
+    ).to("yr")
+    binary.Get_Time_From_Merger(
+        source_2, freq=1 / u.yr, in_frame="observer", out_frame="observer"
+    ).to("yr")
     binary.Get_Mono_Char_Strain(
         source_2,
         np.max(source_2.instrument.T_obs).to("s"),
         inc=np.pi / 2,
-        T_evol_frame="observer",
         f_gw_frame="observer",
     )
-    source_3.Check_Freq_Evol(
-        T_evol=np.max(source_3.instrument.T_obs).to("s"), T_evol_frame="source"
+    binary.Check_Freq_Evol(
+        source_3,
+        T_evol=np.max(source_3.instrument.T_obs).to("s"),
+        T_evol_frame="source",
     )
     binary.Get_Mono_Strain(source_2)
-    binary.Get_Mono_Strain(source_2, frame="observer")
+    binary.Get_Mono_Strain(source_2, out_frame="observer")
 
 
+def test_BBH_Mono_Funcs():
+    source_5 = binary.BBHFrequencyDomain(M[0], q[1], z[1], f_gw=1e-5 * u.Hz)
+    source_5.chi1 = 0.0
+    source_5.chi2 = 0.0
+    source_5.f;
+    source_5.chi2 = 0.5
+    source_5.h_f;
+    binary.Get_Mono_Char_Strain(source_5)
+    binary.Get_Mono_Char_Strain(
+        source_5,
+        method="SPA",
+        freq=source_5.f_gw,
+        f_gw_frame="source",
+        pn_frame="source",
+        out_frame="observer",
+        inc=np.pi / 2,
+    )
+    binary.Get_Mono_Char_Strain(
+        source_5,
+        method="SPA",
+        freq=source_5.f_gw,
+        f_gw_frame="source",
+        pn_frame="source",
+        out_frame="source",
+        inc=np.pi / 2,
+    )
+    binary.Get_Mono_Char_Strain(
+        source_5,
+        method="SPA",
+        freq=source_5.f_gw,
+        f_gw_frame="source",
+        pn_frame="source",
+        out_frame="source",
+        inc=np.pi / 2,
+    )
+    binary.Get_Mono_Char_Strain(
+        source_5,
+        method="SPA",
+        f_gw_frame="observer",
+        pn_frame="source",
+        out_frame="source",
+    )
+    binary.Get_Mono_Char_Strain(
+        source_5,
+        method="PN",
+        freq=source_5.f_gw,
+        f_gw_frame="observer",
+        pn_frame="source",
+        out_frame="source",
+    )
+    binary.Get_Mono_Char_Strain(
+        source_5,
+        method="PN",
+        freq=source_5.f_gw,
+        f_gw_frame="source",
+        pn_frame="observer",
+        out_frame="source",
+    )
+    binary.Get_Mono_Strain(
+        source_5,
+        method="PN",
+        freq=source_5.f_gw,
+        f_gw_frame="source",
+        pn_frame="observer",
+        out_frame="source",
+    )
+    binary.Get_F_Dot(source_5, freq=None, in_frame="observer", out_frame="observer")
+    binary.Get_F_Dot(source_5, freq=None, in_frame="source", out_frame="observer")
+    binary.Get_F_Dot(source_5, freq=None, in_frame="observer", out_frame="source")
+    binary.Get_F_Dot(
+        source_5, freq=source_5.f_gw, in_frame="source", out_frame="source"
+    )
+    binary.Check_Freq_Evol(
+        source_5,
+        T_evol=4*u.yr.to('s'),
+        T_evol_frame="observer",
+        f_gw_frame="source",
+    )
+
+def test_BBH_lalsuite_approx(LISA_prop1):
+    approximant = "IMRPhenomD"
+    lalsuite_kwargs = {"eccentricity": 0.5}
+    delT_obs = (1/4/u.yr)
+    delT_obs = delT_obs.to('1/s')
+
+    source_6 = binary.BBHFrequencyDomain(
+        M, q, z, approximant=approximant, lalsuite_kwargs=lalsuite_kwargs
+    )
+    source_6.f_low = 1e-7
+    source_6.chi1 = 0.5
+    source_6.chi2 = -0.2
+    source_6.f_gw = 1e-3*u.Hz
+    [_,_] = waveform.Get_Waveform(
+        source_6,
+        approximant=approximant,
+        pct_of_peak=0.01,
+        lalsuite_kwargs=lalsuite_kwargs,
+        out_frame="observer")
+    binary.Get_Char_Strain(source_6);
+
+    lalsuite_kwargs.update({'approximant':'IMRPhenomPv3','chi1':0.0,'chi2':0.2,'deltaF':delT_obs.value,
+               'f_min':delT_obs.value,
+               'f_max':delT_obs.value*1e3,'f_ref':0.1*u.Hz,})
+    source_7 = binary.BBHFrequencyDomain(
+        M[0], q[0], z[0], lalsuite_kwargs=lalsuite_kwargs
+    )
+    source_7.f_gw = 1e-3*u.Hz
+    binary.Get_Char_Strain(source_7, in_frame="source", out_frame="observer");
+    binary.Get_Char_Strain(source_7, in_frame="observer", out_frame="source");
+    binary.Get_Char_Strain(source_7, in_frame="source", out_frame="source");
+
+    lalsuite_kwargs_2 = {'m1':M[0]/(1+q[0])*u.M_sun.to('kg'),'m2':(M[0]*q[0]/(1+q[0]))*u.M_sun.to('kg'),
+               'S1x':0, 'S1y':0, 'S1z':x1[2],
+               'S2x':0, 'S2y':0, 'S2z':x2[2],
+               'distance':7.146757481804179e+27,'inclination':0,
+               'phiRef':0,'longAscNodes':0,'eccentricity':0,'meanPerAno':0,'deltaF':delT_obs,
+               'f_min':delT_obs*.01,
+               'f_max':delT_obs*1e3,
+               'f_ref':0,
+               'LALpars':{}}
+    source_8 = binary.BBHFrequencyDomain(
+        M[0], q[0], z[0], approximant=98, instrument=LISA_prop1, lalsuite_kwargs=lalsuite_kwargs_2
+    )
+    [_,_] = waveform.Get_Waveform(
+        source_8,
+        approximant=98,
+        pct_of_peak=0.01,
+        lalsuite_kwargs=lalsuite_kwargs_2,
+        out_frame="source")
+
+    waveform.Strain_Conv(source_8, source_8.f, source_8.h_f, inverse=True, in_frame="source", out_frame="observer");
+    waveform.Strain_Conv(source_8, source_8.f, source_8.h_f, inverse=True, in_frame="observer", out_frame="observer");
+    waveform.Strain_Conv(source_8, source_8.f, source_8.h_f, inverse=True, in_frame="source", out_frame="source");
+    waveform.Strain_Conv(source_8, source_8.f, source_8.h_f, inverse=True, in_frame="observer", out_frame="source");
 # ### Numerical Relativity from EOB subtraction
 
 EOBdiff_filedirectory = gwent.__path__[0] + "/LoadFiles/DiffStrain/EOBdiff/"